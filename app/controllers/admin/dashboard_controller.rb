ab45e3542f071c221ab3e517500548bd735eb682
PavelKotlov <pavelkotlov@gmail.com>
2023-04-09 23:59:49 -0700

    Refactor admin/dashboard view to display current #of categories/products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['username'], password: ENV['password']
 
   def show
+    @products = Product.all
+    @catagories = Category.all
   end
   
 end
